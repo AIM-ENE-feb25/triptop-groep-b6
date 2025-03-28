de34745c2ff98c1bafde0ee3c3f5581601263ee0
Bryan5654 <BF.Velthuizen@student.han.nl>
2025-03-28 10:00:54 +0100

    Design pattern update

---
+++ gemaakte_werk_bryan/Design pattern demo/Systems/Heating.java
@@ -0,0 +1,7 @@
+package org.example.Systems;
+
+public class Heating {
+    public void heatToTemperature(int temperature) {
+        System.out.println("Verwarming ingesteld op " + temperature + " graden.");
+    }
+}
---
+++ gemaakte_werk_bryan/Design pattern demo/Systems/Lighting.java
@@ -0,0 +1,7 @@
+package org.example.Systems;
+
+public class Lighting {
+    public void on() { System.out.println("Lichten worden aangezet."); }
+    public void off() { System.out.println("Lichten worden uitgezet."); }
+    public void dim() { System.out.println("Lichten worden gedimd."); }
+}
