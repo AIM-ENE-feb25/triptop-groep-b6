dd9d81dcc96a74d462ff1482d7a57249104e6df5
Thieme2006 <150018198+Thieme2006@users.noreply.github.com>
2025-03-31 13:52:27 +0200

    Backend toegevoegd

---
+++ triptop/src/test/java/com/prototype/triptop/TriptopPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.triptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptopPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
