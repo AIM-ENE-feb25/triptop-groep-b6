3386067bb2bbafe7836086a2667af185cb426315
Daaanieell <151017261+Daaanieell@users.noreply.github.com>
2025-03-31 14:27:04 +0200

    controller "works"

--- triptop/src/main/java/com/prototype/triptop/controller/PaymentController.java
+++ triptop/src/main/java/com/prototype/triptop/controller/PaymentController.java
@@ -1,9 +1,11 @@
 package com.prototype.triptop.controller;
 
 
+import com.prototype.triptop.domain.Payment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/payment")
 public class PaymentController {
     @PostMapping("/process")
-    public ResponseEntity processPayment() {
-        return null;
+    public ResponseEntity processPayment(@RequestBody Payment payment) {
+        return ResponseEntity.ok(payment.toString());
     }
 
 }
