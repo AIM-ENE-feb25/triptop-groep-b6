40810550be7688b179bc49896ffbcad34a84847f
Daaanieell <151017261+Daaanieell@users.noreply.github.com>
2025-04-02 21:10:56 +0200

    removed unused imports + styling

--- triptop/src/main/java/com/prototype/triptop/adapter/PaymentAdapterInterface.java
+++ triptop/src/main/java/com/prototype/triptop/adapter/PaymentAdapterInterface.java
@@ -2,7 +2,6 @@ package com.prototype.triptop.adapter;
 
 import com.prototype.triptop.domain.Payment;
 import org.springframework.http.ResponseEntity;
-
 import java.io.IOException;
 
 public interface PaymentAdapterInterface {
--- triptop/src/main/java/com/prototype/triptop/exception/GlobalExceptionHandler.java
+++ triptop/src/main/java/com/prototype/triptop/exception/GlobalExceptionHandler.java
@@ -1,27 +1,28 @@
-    package com.prototype.triptop.exception;
+package com.prototype.triptop.exception;
 
-    import org.springframework.http.HttpStatus;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.web.bind.annotation.ControllerAdvice;
-    import org.springframework.web.bind.annotation.ExceptionHandler;
-    import java.util.HashMap;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-    @ControllerAdvice
-    public class GlobalExceptionHandler {
-        public HashMap<String, String> createErrorResponse(String title, String error) {
-            HashMap<String, String> errorResponse = new HashMap<>();
-            errorResponse.put("title", title);
-            errorResponse.put("error", error);
-            return errorResponse;
-        }
+import java.util.HashMap;
 
-        @ExceptionHandler(InvalidPaymentException.class)
-        public ResponseEntity<HashMap<String, String>> handleInvalidPaymentException(InvalidPaymentException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createErrorResponse("InvalidPaymentException", ex.getMessage()));
-        }
-
-        @ExceptionHandler(PaymentRequestException.class)
-        public ResponseEntity<HashMap<String, String>> handlePaymentFailedException(PaymentRequestException ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(createErrorResponse("PaymentRequestException", ex.getMessage()));
-        }
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    public HashMap<String, String> createErrorResponse(String title, String error) {
+        HashMap<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("title", title);
+        errorResponse.put("error", error);
+        return errorResponse;
     }
+
+    @ExceptionHandler(InvalidPaymentException.class)
+    public ResponseEntity<HashMap<String, String>> handleInvalidPaymentException(InvalidPaymentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createErrorResponse("InvalidPaymentException", ex.getMessage()));
+    }
+
+    @ExceptionHandler(PaymentRequestException.class)
+    public ResponseEntity<HashMap<String, String>> handlePaymentFailedException(PaymentRequestException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(createErrorResponse("PaymentRequestException", ex.getMessage()));
+    }
+}
